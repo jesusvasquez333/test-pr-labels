03afd51afcedfe7d87003b06e8c4bbf4f27943bb
Jesus Vasquez <jesusvasquez333@gmail.com>
2020-09-10 18:08:33 -0700

    Remove debug messages

--- verify_pr_lables.py
+++ verify_pr_lables.py
@@ -75,7 +75,6 @@ for label in pr_labels:
 was_approved = None
 for review in pr_reviews.reversed:
     if review.user.login == 'github-actions[bot]':
-        print(f'Last review found: status = {review.state}')
         if review.state == 'APPROVED':
             was_approved = True
         elif review.state == 'CHANGES_REQUESTED':
@@ -93,8 +92,6 @@ for review in pr_reviews.reversed:
 # Instead, we will create a pull request review, marked with 'REQUEST_CHANGES' when no valid label was found.
 # This will prevent merging the pull request until a valid label is added, which will trigger this check again
 # and will create a new pull request review, but in this case marked as 'APPROVE'
-
-print(f'was_approved = {was_approved}')
 
 if len(pr_valid_labels):
     # If there were valid labels, create a pull request review, approving it
